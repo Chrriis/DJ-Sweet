aa4452aac09b5333ca08ed5fba3af9e5ce9208a7
Chrriis <chrriis@gmail.com>
2009-07-21 20:05:14 +0000

    Flash player documentation is improved.

--- DJSweet/src/chrriis/dj/sweet/components/FlashPlayerListener.java
+++ DJSweet/src/chrriis/dj/sweet/components/FlashPlayerListener.java
@@ -14,6 +14,10 @@ import org.eclipse.swt.internal.SWTEventListener;
  */
 public interface FlashPlayerListener extends SWTEventListener {
 
+  /**
+   * The Flash player can invoke special commands to the application simply by calling:<br>
+   * <code>ExternalInterface.call("sendCommand", commandName, arg1, arg2, ...);</code>
+   */
   public void commandReceived(String command, String[] args);
 
 }
