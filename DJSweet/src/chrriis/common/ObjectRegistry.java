40bdfc82cc0128dd77ae8c06d1159d3db094f485
Chrriis <chrriis@gmail.com>
2010-09-14 17:02:49 +0000

    Registry is reviewed to not create too many threads when the last item is removed/added too fast.

--- DJSweet/src/chrriis/common/ObjectRegistry.java
+++ DJSweet/src/chrriis/common/ObjectRegistry.java
@@ -28,7 +28,7 @@ public class ObjectRegistry {
       public void run() {
         while(true) {
           try {
-            sleep(5000);
+            sleep(1000);
           } catch(Exception e) {
           }
           synchronized(ObjectRegistry.this) {
@@ -113,10 +113,6 @@ public class ObjectRegistry {
    */
   public synchronized void remove(int instanceID) {
     instanceIDToObjectReferenceMap.remove(instanceID);
-    if(instanceIDToObjectReferenceMap.isEmpty() && cleanUpThread != null) {
-      cleanUpThread.interrupt();
-      cleanUpThread = null;
-    }
   }
 
   /**
